41a51c11200a3954abf8a7851b0067076cbc57dc
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-09 12:55:23 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,12 @@
 str1='''Peechle buttis saal se inhone nirantar is college mein chamatkar pe chamatkar kiye. 
 Umeed hai aagey bee karte rahenge Hamein to aashcharya hota hai ki ek insaan apne jeevan kaal 
 mein itni chamatkar kaisi kar sakta hai'''
-str= str1.replace("chamatkar","balatkar")
+str= str1.replace("chamatkar","balatkar") # 1.(old,new)  2. save it to another variable 3. in "List" we dont have to store it somewhere else
 print(str)
+
+
+# OUTPUT:
+ 
+# Peechle buttis saal se inhone nirantar is college mein balatkar pe balatkar kiye.
+# Umeed hai aagey bee karte rahenge Hamein to aashcharya hota hai ki ek insaan apne jeevan kaal
+# mein itni balatkar kaisi kar sakta hai
