61d3c595b85ac8a40bc28b84e59695bb5fbc8009
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-08-09 12:34:27 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+str1='''Peechle buttis saal se inhone nirantar is college mein chamatkar pe chamatkar kiye. 
+Umeed hai aagey bee karte rahenge Hamein to aashcharya hota hai ki ek insaan apne jeevan kaal 
+mein itni chamatkar kaisi kar sakta hai'''
+str= str1.replace("chamatkar","balatkar")
+print(str)
